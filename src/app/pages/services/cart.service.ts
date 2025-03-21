b656bb1497b42c896ca2e72d374f970abe332966
marco3islas <marco3islas@gmail.com>
2025-03-21 11:48:20 -0600

    Modificadas las imagenes para mejorar el performance

--- src/app/pages/services/cart.service.ts
+++ src/app/pages/services/cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable } from '@angular/core';
 import { Tenis } from '../interfaces/tenis';
 
 @Injectable({
@@ -6,6 +6,18 @@ import { Tenis } from '../interfaces/tenis';
 })
 export class CartService {
   private cartItems: Tenis[] = [];
+
+  constructor() {
+    this.loadFromLocalStorage();
+  }
+
+  loadFromLocalStorage(): void {
+    const savedCartItems = localStorage.getItem('cartItems');
+    if (savedCartItems) {
+      this.cartItems = JSON.parse(savedCartItems);
+    }
+  }
+
 
   addItem(item: Tenis): void {
     this.cartItems.push(item);
@@ -27,4 +39,9 @@ export class CartService {
   totalAmount(): number {
     return this.cartItems.reduce((acc, item) => acc + (item.precio * 1 || 0), 0); // reduce requiere una variable de acumulador (acc) que es el que representa las veces que se va a recorrer el array de acuerdo a su longitud, y el item que es el que representa cada elemento del array. Se establece una funcion de flecha que va a sumar el precio de cada item (el cual se multiplica por 1 para convertirlo a número) y se le asigna un valor inicial de 0.
   }
+
+  saveToLocalStorage(): void {
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
 }
