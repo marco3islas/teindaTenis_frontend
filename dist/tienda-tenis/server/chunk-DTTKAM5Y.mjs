4c3c00e11d23b2a8512b077ec5f1d7e1280a63b7
Marco Islas <marco3islas@gmail.com>
2025-04-30 21:00:50 -0600

    Modificado el archivo angular json para hacer prerenders y el archivo environment

---
+++ dist/tienda-tenis/server/chunk-DTTKAM5Y.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{d as a}from"./chunk-7W7B2CID.mjs";import"./chunk-JVPK42KC.mjs";import"./chunk-OEQLWKHH.mjs";import"./chunk-DCFYTP2A.mjs";export{a as TenisModule};
