e74bf554aed224a1aa00f15b79d2404fb400ab5b
Marco Islas <marco3islas@gmail.com>
2025-05-01 21:55:11 -0600

    Modificado el archvivo cart.service para prevenir los datos distintos a json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://tiendatenisbackend-production.up.railway.app',
+  apiUrl: 'https://tiendatenisbackend-production.up.railway.app/',
 }
